40a670a7d5536e69fb9c0afc6bf11c3500ecf229
zotherstupidguy <zotherstupidguy@gmail.com>
2016-12-21 22:01:06 +0200

    adds specs

--- lib/mushin_ext_github.rb
+++ lib/mushin_ext_github.rb
@@ -1,27 +1,48 @@
-      require 'mushin'
-      require_relative 'Github/version'
-
+require 'mushin'
+require 'octokit'
+require_relative 'Github/version'
 
 module Github
-        class Ext 
-        using Mushin::Ext 
+  class Ext 
+    using Mushin::Ext 
 
-	def initialize app=nil, opts={}, params={}
-	  @app 		= app
-	  @opts 	= opts
-	  @params 	= params 
+    def initialize app=nil, opts={}, params={}
+      @app 	= app
+      @opts 	= opts
+      @params 	= params 
+    end
+    def call env 
+      env ||= Hash.new 
+
+      case @opts[:cqrs]
+      when :cqrs_query 
+	#inbound code
+	@app.call(env)
+	#outbound code
+      when :cqrs_command
+	#inbound code
+	@client 	= Octokit::Client.new(:login => @opts[:auth_username], :password => @opts[:auth_password])
+	user 		= @client.user
+	env[:login] 	= user.login
+
+	#TODO in case of private repos the clone_url should be prefixed with a token and passed to other domain extenstions to handle the local work
+	if !@params[:username].nil? && !@params[:reponame].nil? then 
+	  @slug 		= @params[:username] + "/" + @params[:reponame] 
+	  env[:slug]		= @slug
+	  env[:repo_metadata]	= @client.repo(@slug).to_h
+	  env[:clone_url] 	= @client.repo(@slug).clone_url
+	elsif !@params[:slug].nil? then 
+	  @slug 		= @params[:slug] 
+	  env[:slug]		= @slug
+	  env[:repo_metadata]	= @client.repo(@slug).to_h
+	  env[:clone_url] 	= @client.repo(@slug).clone_url
 	end
-	def call env 
-	  env ||= Hash.new 
-	  # write inbound code
-	  if @opts[:cqrs] == :cqrs_query then
-	    # write your code here if it is a cqrs query
-	  else 
-	    # write your code here if it is a cqrs command 
-	  end
-	  @app.call(env)
-	  # write outbound code
-	end
+	@app.call(env)
+	#outbound code
+      else
+	raise "you must specifiy if your cqrs call is command or query?"
       end
+    end
+  end
 
 end
--- spec/Github_spec.rb
+++ spec/Github_spec.rb
@@ -1,42 +1,50 @@
 require_relative 'spec_helper'
 require 'yaml'
 require 'octokit'
-require 'rugged'
 
 describe "Github" do
-  before do
-    # flush all previous test data
-    `rm -rf  DATA`
+
+  before do 
+    @env 		= {} 
+
     @secrets 		= YAML::load_file "secrets.yml"
-    @client 		= Octokit::Client.new :login    => @secrets["github"]["login"], :password => @secrets["github"]["password"]
-    @username 		= 'pengwynn'
-    @reponame 		= 'pingwynn'
-    @slug 		= "#{@username}/#{@reponame}"
-    @repo_url 		= @client.repo(@slug).git_url
-    @local_repo_path 	= ("./DATA" + "/" + @slug)
-  end 
+
+    @username 		= "pengwynn"
+    @reponame 		= "pingwynn"
+    @slug 		= @username + "/" + @reponame 
+  end
+
+  after do 
+  end
 
   it "accesses github account via username and password" do
-    user = @client.user
-    user.login.must_equal @secrets["github"]["login"]
+    opts 	= {:cqrs => :cqrs_command, :auth_username => @secrets["github"]["login"], :auth_password => @secrets["github"]["password"]}
+    params 	= {}
+    @ext 	= Github::Ext.new(Proc.new {}, opts, params)
+
+    @env[:login].must_be_nil
+    @ext.call(@env)
+    @env[:login].must_equal @secrets["github"]["login"]
   end 
 
-  it "takes a repo name and return its metadata" do
-    repo = @client.repo @slug 
-    repo.to_hash.empty?.must_equal false 
+  it "takes a username/reponame or a slug and return the metadata of its repo" do
+    opts 	= {:cqrs => :cqrs_command, :auth_username => @secrets["github"]["login"], :auth_password => @secrets["github"]["password"]}
+    params 	= {:username => @username, :reponame => @reponame, :slug => @slug}
+    @ext 	= Github::Ext.new(Proc.new {}, opts, params)
+
+    @env[:repo_metadata].must_be_nil
+    @ext.call(@env)
+    @env[:repo_metadata].empty?.must_equal false 
   end
 
-  it "takes a repo slug and return its clone_url" do
-    repo = @client.repo @slug
-    repo.clone_url.must_equal "https://github.com/#{@slug}.git"
+  it "takes a username/reponame or a slug and return the clone_url of its repo" do
+    opts 	= {:cqrs => :cqrs_command, :auth_username => @secrets["github"]["login"], :auth_password => @secrets["github"]["password"]}
+    params 	= {:username => @username, :reponame => @reponame, :slug => @slug}
+    @ext 	= Github::Ext.new(Proc.new {}, opts, params)
+
+    @env[:clone_url].must_be_nil
+    @ext.call(@env)
+    @env[:clone_url].must_equal "https://github.com/#{@slug}.git"
   end
 
-  it "takes a repo slug and clones it locally" do
-    Rugged::Repository.clone_at(@repo_url, @local_repo_path, {
-      transfer_progress: lambda { |total_objects, indexed_objects, received_objects, local_objects, total_deltas, indexed_deltas, received_bytes|
-	print("\r total_objects: #{total_objects}, indexed_objects: #{indexed_objects}, received_objects: #{received_objects}, local_objects: #{local_objects}, total_deltas: #{total_deltas}, indexed_deltas: #{indexed_deltas}, received_bytes: #{received_bytes}")
-      }   
-    })  
-    File.directory?(@local_repo_path).must_equal true 
-  end
-end 
+end
