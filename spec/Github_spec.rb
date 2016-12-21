c815873eda7b433bbeb404b92fd45c2e45b05cb4
zotherstupidguy <zotherstupidguy@gmail.com>
2016-12-21 13:31:32 +0200

    init

---
+++ spec/Github_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+class GithubTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Github::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
